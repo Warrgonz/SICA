8a914cefb83d62647870d09cd15df021a251c687
acast <acast@LAPTOP-KJ3MAPEO>
2023-06-10 20:47:49 -0600

    Creacion de proyecto

---
+++ src/main/java/com/SICA/SicaApplication.java
@@ -0,0 +1,13 @@
+package com.SICA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicaApplication.class, args);
+	}
+
+}
