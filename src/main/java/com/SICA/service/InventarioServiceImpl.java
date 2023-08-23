3b5f718aa81c1cd0854e855b8a2991c13ae56265
acast <acast@LAPTOP-KJ3MAPEO>
2023-08-23 00:37:44 -0600

    cambio Dao para probleams version

--- src/main/java/com/SICA/service/InventarioServiceImpl.java
+++ src/main/java/com/SICA/service/InventarioServiceImpl.java
@@ -1,19 +1,19 @@
 
 package com.SICA.service;
 
-import com.SICA.AccesoDatos.InventarioDao;
 import com.SICA.domain.Inventario;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.SICA.AccesoDatos.InventarioDao2;
 
 
 @Service
 public class InventarioServiceImpl implements InventarioService {
     
     @Autowired
-    InventarioDao inventarioDao;
+    InventarioDao2 inventarioDao;
     
     @Transactional(readOnly = true) //Para manejar de lectura
     @Override
