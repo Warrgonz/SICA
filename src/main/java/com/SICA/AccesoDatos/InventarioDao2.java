59fcbf1e6dba4bff0740766561bf5ffcd10535ec
acast <acast@LAPTOP-KJ3MAPEO>
2023-08-23 00:35:03 -0600

    cambio Dao para probleams version

---
+++ src/main/java/com/SICA/AccesoDatos/InventarioDao2.java
@@ -0,0 +1,10 @@
+
+package com.SICA.AccesoDatos;
+
+import com.SICA.domain.Inventario;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface InventarioDao2 extends CrudRepository<Inventario, Long>{
+    
+}
